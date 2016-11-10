3236bca2093ef1429808388031cf7c9db3274f09
ailyntong <athappiness@gmail.com>
2016-11-09 21:31:49 -0800

    fixed operator overloading

---
+++ Point.cpp
@@ -0,0 +1,13 @@
+#include "Point.h"
+
+Point operator + (const Point &lhs, const Point &rhs) {
+	return Point{ lhs.x + rhs.x, lhs.y + rhs.y };
+}
+
+Point operator - (const Point &lhs, const Point &rhs) {
+	return Point{ lhs.x - rhs.x, lhs.y - rhs.y };
+}
+
+Point operator * (const Point point, const float mult) {
+	return Point{ point.x * mult, point.y * mult };
+}
--- Point.h
+++ Point.h
@@ -11,16 +11,10 @@ struct Point {
 	}
 };
 
-Point operator + (const Point &lhs, const Point &rhs) {
-	return Point{ lhs.x + rhs.x, lhs.y + rhs.y };
-}
+Point operator + (const Point &lhs, const Point &rhs);
 
-Point operator - (const Point &lhs, const Point &rhs) {
-	return Point{ lhs.x - rhs.x, lhs.y - rhs.y };
-}
+Point operator - (const Point &lhs, const Point &rhs);
 
-Point operator * (const Point point, const float mult) {
-	return Point{ point.x * mult, point.y * mult };
-}
+Point operator * (const Point point, const float mult);
 
 #endif
