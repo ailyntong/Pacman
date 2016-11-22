b37c4576c06ec20016fcbd6be5475609c7c343b5
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-11-21 16:53:04 -0800

    finished operators for Point

--- Point.cpp
+++ Point.cpp
@@ -8,10 +8,29 @@ Point operator - (const Point &lhs, const Point &rhs) {
 	return Point{ lhs.x - rhs.x, lhs.y - rhs.y };
 }
 
+bool operator==(const Point & lhs, const Point & rhs) {
+	return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+bool operator!=(const Point & lhs, const Point & rhs) {
+	return !(lhs == rhs);
+}
+
 Point operator * (const Point point, const float mult) {
 	return Point{ point.x * mult, point.y * mult };
 }
 
 Point operator*(const float mult, const Point point) {
 	return point * mult;
+}
+
+Point operator/(const Point & lhs, float denominator) {
+	if(denominator == 0) {
+		throw "Divide by Zero";
+	}
+	return (1 / denominator) * lhs;
+}
+
+Point Point::operator-() const {
+	return Point{-x, -y};
 }
